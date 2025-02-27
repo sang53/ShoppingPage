7e40f41fb95d65c8a988d12a49618c34e40ab5af
sang53 <143582855+sang53@users.noreply.github.com>
2025-02-28 00:57:56 +1100

    add arrows for sorting order

--- src/components/Display/SortSelect/SortSelect.jsx
+++ src/components/Display/SortSelect/SortSelect.jsx
@@ -9,11 +9,49 @@ export default function SortSelect({ by, order, setSort }) {
         <option value="price">Price</option>
       </select>
       <button onClick={() => setSort([by, order === "asc" ? "desc" : "asc"])}>
-        {order}
+        {order === "asc" ? upArrow : downArrow}
       </button>
     </div>
   );
 }
+
+const upArrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 5l0 14" />
+    <path d="M18 11l-6 -6" />
+    <path d="M6 11l6 -6" />
+  </svg>
+);
+
+const downArrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 5l0 14" />
+    <path d="M18 13l-6 6" />
+    <path d="M6 13l6 6" />
+  </svg>
+);
 
 SortSelect.propTypes = {
   by: PropTypes.string,
